a5d7c3c484e2da188ef15e5161c38e469e714e4a
leroydemon <104576853+leroydemon@users.noreply.github.com>
2024-09-04 16:07:02 +0300

    add entityframework

---
+++ backend/Domain/Data/TaskAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.DbConfiguration;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Data
+{
+    public partial class TaskAppDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public TaskAppDbContext(DbContextOptions<TaskAppDbContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new EntityConfiguration());
+            modelBuilder.ApplyConfiguration(new TaskToDoConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
---
+++ backend/Domain/DbConfiguration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.DbConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            //Identity have default setting for property and we can configure here or in IdentityOptions
+            builder
+                .Property(x => x.UserName)
+                .IsRequired();
+
+            builder
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+
+            builder
+                .Property(x => x.PasswordHash)
+                .IsRequired();
+        }
+    }
+}
