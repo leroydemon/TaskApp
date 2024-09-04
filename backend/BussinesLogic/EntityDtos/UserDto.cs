67b09871a36a4c5db04324ef65047522386379b5
leroydemon <104576853+leroydemon@users.noreply.github.com>
2024-09-04 16:34:32 +0300

    add repository and DTO

---
+++ backend/BussinesLogic/EntityDtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BussinesLogic.EntityDtos
+{
+    public class UserDto : IdentityUser<Guid>
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ backend/Domain/Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Data;
+using Domain.Interfaces;
+
+namespace Domain.Repository
+{
+    //Generic repository for successful scaling and non-repeat code
+    public class Repository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly TaskAppDbContext _context;
+        public Repository(TaskAppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
